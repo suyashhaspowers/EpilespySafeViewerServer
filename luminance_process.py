8e6e0b9909243620772b55aa5dfb03e21ae65069
suyashhaspowers <suyashhaspowers@gmail.com>
2021-08-21 17:45:14 -0400

    feat(backend): anomaly process + route for frontend

--- luminance_process.py
+++ luminance_process.py
@@ -59,27 +59,3 @@ def post_process_luminance(lums, fps):
         moving_sum = sum(lums_thresh_absgrad[max(0, i - sliding_window_size//2) : i + 1 + sliding_window_size//2])
         lums_thresh_absgrad_movingsum.append(moving_sum)
     return lums_thresh_absgrad_movingsum
-
-def post_process_anomalies(fps, anomalies):
-    anomalies_bucketed = []
-    sliding_window_size = 10
-    num_points = len(anomalies)
-    
-    for i in range(num_points):
-        bucket_val = any(anomalies[max(0, i - sliding_window_size) : i])
-        anomalies_bucketed.append(bucket_val)
-    return anomalies_bucketed
-
-def get_changes(anomalies):
-    changes = []
-    prev = None
-    for indx, val in enumerate(anomalies):
-        if prev != val:
-            changes.append([indx, val])
-            prev = val
-    return changes
-
-
-def get_changes_in_video_time(changes, fps):
-    timePerFrame = 1.0/fps
-    return [[change[0]*timePerFrame, change[1]] for change in changes]
