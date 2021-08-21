fae429e22234306913ebd808dbe193754dba6637
suyashhaspowers <suyashhaspowers@gmail.com>
2021-08-21 01:19:24 -0400

    feat(backend): luminance data + post-processing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import luminance_process
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    luminance_process.download_youtube_video("https://www.youtube.com/watch?v=TDjamEAMIgo&ab_channel=Simon%26TaliaClips")
+    lums = (luminance_process.analyse_video_luminance('./videos/db2b281fc64d5b778d351e6d453e3b7c.mp4'))
+    print(luminance_process.post_process_luminance(lums, 30))
     return "Epilepsy Safe Viewer"
