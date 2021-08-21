334d5e39c9b1c5b5ee508512acb33ffe8e2060be
sayohnahilan <sayohnahilan@gmail.com>
2021-08-21 03:27:08 -0400

    move-to-azure

--- azure_client.py
+++ azure_client.py
@@ -16,7 +16,7 @@ def getAnomalies(lums):
 
     for timestamp, lum in zip(timestamps, lums):
         series.append(TimeSeriesPoint(timestamp=timestamp, value=lum))
-    request = DetectRequest(series=series, granularity='secondly', max_anomaly_ratio=0.49, sensitivity=95)
+    request = DetectRequest(series=series, granularity='secondly', max_anomaly_ratio=0.49, sensitivity=99)
 
     response = None
     try:
--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask
 import luminance_process
 import azure_client
 
+app = Flask(__name__)
 
-luminance_process.download_youtube_video("https://www.youtube.com/watch?v=TDjamEAMIgo&ab_channel=Simon%26TaliaClips")
-lums = (luminance_process.analyse_video_luminance('./videos/db2b281fc64d5b778d351e6d453e3b7c.mp4'))
-print("luminance values:", luminance_process.post_process_luminance(lums, 30))
 
-print("anomalies:", azure_client.getAnomalies(lums))
+@app.route('/')
+def hello():
+    file = luminance_process.download_youtube_video("https://www.youtube.com/watch?v=atkD-beZ9oI")
+    lums = (luminance_process.analyse_video_luminance(file))
+
+    lums_post = luminance_process.post_process_luminance(lums, 30)
+    anomalies = azure_client.getAnomalies(lums)
+
+    for i in range(len(lums_post) - 1):
+        print(lums_post[i], anomalies[i])
